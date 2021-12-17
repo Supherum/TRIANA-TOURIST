f9736ef6875469cc5d5140273b19c2050c1404ee
repiso.sapab21 <repiso.sapab21@triana.salesianos.edu>
2021-12-17 13:35:06 +0100

    first commit

---
+++ triana-tourist/src/main/java/com/proyecto/trianatourist/TrianaTouristApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.trianatourist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrianaTouristApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrianaTouristApplication.class, args);
+	}
+
+}
---
+++ triana-tourist/src/test/java/com/proyecto/trianatourist/TrianaTouristApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.trianatourist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrianaTouristApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
